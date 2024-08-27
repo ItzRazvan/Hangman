da7918c43609cfe712419f3b7b0ba57bc3891377
ItzRazvan <cisteianrazvan@gmail.com>
2024-08-27 23:46:01 +0300

    made the selection of a random word

--- hangman.go
+++ hangman.go
@@ -1,8 +1,11 @@
+package main
+
 import (
 	"fmt"
+	"os"
 )
 
-const int MAX_TRIES = 6
+const MAX_TRIES int = 6
 
 type Hangman struct {
 	word    string
@@ -10,9 +13,32 @@ type Hangman struct {
 	tries   int
 }
 
-Hangman hangman
+var hangman Hangman
+
+func generate_random_word() string {
+	//randInt := rand.IntN(10000) + 1
+
+	dat, err := os.ReadFile("word_list.txt")
+	if err != nil {
+		fmt.Println("Couldn't open word list")
+		os.Exit(1)
+	}
+
+	word_list := string(dat[:])
+
+	for char, s := range word_list {
+		if char == '\n' || s == '\n' {
+			fmt.Println("nice")
+		}
+	}
+
+	return ""
+}
 
 func main() {
 	fmt.Println("Welcome to the Hangman game\n\n")
+
+	word := generate_random_word()
+	hangman.word = word
 
 }
